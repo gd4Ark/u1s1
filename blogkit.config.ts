276fb13ddfe40484dcdc9cf8b64ccc74669a753f
4Ark <gd.4ark@gmail.com>
2023-02-22 23:37:18 +0800

    chore: update site config

--- blogkit.config.ts
+++ blogkit.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from "blogkit";
-import { request } from "blogkit-notion";
-import { theme } from "blogkit-theme-minimal";
+import { defineConfig } from 'blogkit'
+import { request } from 'blogkit-notion'
+import { theme } from 'blogkit-theme-minimal'
 
 export default defineConfig({
   siteConfig: {
-    title: "Blogkit Notion",
-    author: "2nthony",
-    url: "https://blogkit.vercel.app",
+    title: 'u1s1',
+    author: 'u1s1',
+    url: 'https://u1s1.me'
   },
   theme,
-  request,
-});
+  themeConfig: {
+    links: [
+      {
+        name: 'RSS',
+        url: '/api/rss'
+      }
+    ]
+  },
+  request
+})
